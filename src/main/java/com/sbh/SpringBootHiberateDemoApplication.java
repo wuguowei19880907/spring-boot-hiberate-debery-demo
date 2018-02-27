dfa38ae88b4ce1c809de28ad8bf3d73d45536479
wuguowei19880907 <wuguowei.34@163.com>
2018-02-27 18:12:05 +0800

    init product

---
+++ src/main/java/com/sbh/SpringBootHiberateDemoApplication.java
@@ -0,0 +1,12 @@
+package com.sbh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHiberateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHiberateDemoApplication.class, args);
+	}
+}
